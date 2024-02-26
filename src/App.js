60c50f171bb7b07160c153072937859f038ff7fe
Srija <srijapaul078@gmail.com>
2024-02-26 22:52:08 +0530

    error showing

---
+++ src/App.js
@@ -0,0 +1,100 @@
+
+import './App.css';
+import Alert from './components/Alert';
+//import About from './components/About';
+import Navbar from './components/Navbar';
+import TextArea from './components/TextArea';
+import React, { useState } from 'react';
+//import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+
+
+function App() {
+  const [mode,setmode]=useState("light");
+  const [alert,setalert]=useState(null);
+
+  const showAlert=(message,type)=>{
+    setalert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setalert(null);
+    }, 1500);
+  }
+  const removeBodyClasses=()=>{
+    document.body.classList.remove('bg-primary');
+    document.body.classList.remove('bg-danger');
+    document.body.classList.remove('bg-success');
+    document.body.classList.remove('bg-warning');
+    document.body.classList.remove('bg-light');
+    document.body.classList.remove('bg-dark');
+  }
+
+  const togglemode=(cls)=>{
+    removeBodyClasses();
+    document.body.classList.add('bg'+cls);
+    if(mode==="light"){
+      setmode("dark");
+      document.body.style.backgroundColor="darkslategrey";
+      document.body.style.color="#61dafb";
+      showAlert("Dark mode has been enabled","success");
+    }
+    else{
+      setmode("light");
+      document.body.style.backgroundColor="#61dafb";
+      document.body.style.color="darkslategrey";
+      showAlert("Light mode has been enabled","success");
+    }
+  }
+  return (
+    <>
+   
+      {/* <BrowserRouter> */}
+        <Navbar
+          title="TextDose"
+        //  aboutText="About"
+          mode={mode}
+          togglemode={togglemode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-4" mode={mode}>
+        <TextArea showAlert={showAlert} textarea="Enter your text" />
+        </div>
+          {/* <Routes>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route
+              exact path="/"
+              element={
+                <TextArea showAlert={showAlert} textarea="Enter your text" />
+              }
+            ></Route>
+          </Routes>
+        </div>
+      </BrowserRouter> */}
+    </> 
+  );
+      {/* <Router>
+      <Navbar title="TextDose" mode={mode} togglemode={togglemode}/>
+      <Alert alert={alert} />
+      <div className="container">
+      <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+          <TextArea showAlert={showAlert} textarea="Enter your text" />
+          </Route>
+        </Switch>
+        </div>
+        </Router> */}
+      
+      {/* <About /> */}
+    {/* </div> */}
+    
+  // );
+}
+
+
+export default App;
---
+++ src/components/TextArea.js
@@ -0,0 +1,69 @@
+import React,{useState} from 'react'
+
+export default function TextArea(props) {
+    const[text,setText]=useState("");
+    const handleUpclick=()=>{
+        let newText=text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to Uppercase","success");
+    }
+    const handleDownclick=()=>{
+      let newText=text.toLowerCase();
+      setText(newText);
+      props.showAlert("Converted to Lowercase","success");
+  }
+  const handleClearclick=()=>{
+    let newText="";
+    setText(newText);
+    props.showAlert("Text Cleared","success");
+  }
+  const handleCapclick=()=>{
+    let newText=text.toLowerCase().split(' ').map((word)=>word.charAt(0).toUpperCase()+word.slice(1)).join(' ');
+    setText(newText);
+    props.showAlert("Capitalized First Letters","success");
+  }
+  const handleInverseclick=()=>{
+    let newText=text.split('').map(char=> char === char.toUpperCase()?char.toLowerCase():char.toUpperCase()).join('');
+    setText(newText);
+    props.showAlert("Inversed Text","success");
+  }
+
+  const handleRemoveExtraSpaces=()=>{
+    let newText=text.split(/[ ]+/);
+    setText(newText.join(' '));
+    props.showAlert("Extra Spaces Removed","success");
+  }
+
+
+  const handleOnchange=(event)=>{
+    setText(event.target.value);
+}
+    
+  return (
+    <>
+    <h2> Try TextDose to manipulate your texts </h2>
+    <div className="container"  >
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.Textarea}</label>
+        <textarea className="form-control" value={text} onChange={handleOnchange} id="exampleFormControlTextarea1" rows="8"></textarea>
+      </div>
+      <button disabled={text.length===0} className='btn btn-primary mx-2 my-2' onClick={handleUpclick}>UPPERCASE</button>
+      <button disabled={text.length===0} className='btn btn-primary mx-2 my-2' onClick={handleDownclick}>lowercase</button>
+      <button disabled={text.length===0} className='btn btn-primary mx-2 my-2' onClick={handleClearclick}>Clear</button>
+      <button disabled={text.length===0} className='btn btn-primary mx-2 my-2' onClick={handleCapclick}>Capitalize First Letters </button>
+      <button disabled={text.length===0} className='btn btn-primary mx-2 my-2' onClick={handleInverseclick}>iNVERSE</button>
+      <button disabled={text.length===0} className='btn btn-primary mx-2 my-2' onClick={handleRemoveExtraSpaces}>Remove Extra Spaces</button>
+      
+      
+    </div><div className='container'>
+        <h1>Your text summary</h1>
+        <p> {text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+    
+        <p>
+  {text.trim() === "" ? "nothing to read as of now" : `${0.008 *text.split(" ").filter((element) => element.trim().length !== 0).length} minutes to read`}
+</p>
+       {/* <p> {0.008 * text.split(" ").length} minutes to read</p> */}
+
+      </div></>
+  )
+}
